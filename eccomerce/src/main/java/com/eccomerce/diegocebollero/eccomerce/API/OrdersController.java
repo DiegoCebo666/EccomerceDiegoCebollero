beb533ce872dd131fcdc2f894fda577400fe5912
Diego <diegocebo7@gmail.com>
2020-11-24 21:09:36 +0100

    Get Products and quantity from orders by id

--- eccomerce/src/main/java/com/eccomerce/diegocebollero/eccomerce/API/OrdersController.java
+++ eccomerce/src/main/java/com/eccomerce/diegocebollero/eccomerce/API/OrdersController.java
@@ -4,20 +4,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import com.eccomerce.diegocebollero.eccomerce.Model.Order;
+import com.eccomerce.diegocebollero.eccomerce.Model.OrderProduct;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrdersController {
-    private static ArrayList<Order> orders = new ArrayList<>(Arrays.asList(
-        new Order( "01/01/2020", "PIPO", 2),
-        new Order( "02/01/2020", "PIPO", 2),
-        new Order( "03/01/2020", "PIPO", 2)
-    ));
+    private static ArrayList<Order> orders = new ArrayList<>(Arrays.asList(new Order("01/01/2020", "PIPO", 2),
+            new Order("02/01/2020", "PIPO", 2), new Order("03/01/2020", "PIPO", 2)));
+
+    private static ArrayList<OrderProduct> orderProducts = new ArrayList<>(
+            Arrays.asList(new OrderProduct(1, 1, 5), new OrderProduct(2, 4, 7281), new OrderProduct(3, 2, 39)));
 
     @GetMapping("orders")
     public ArrayList<Order> getOrders() {
         return orders;
+    }
+
+    @GetMapping("orders/{id}")
+    public ArrayList<OrderProduct> getOrderProductsById(@PathVariable(name = "id", required = true) int id) {
+        ArrayList<OrderProduct> result = new ArrayList<>();
+        for (int i = 0; i < orderProducts.size(); i++) {
+            if (orderProducts.get(i).getIdorder() == id)
+                result.add(orderProducts.get(i));
+        }
+        return result;
     }
 }
